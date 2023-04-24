d87a4ac9b3e7f42d91fc20910f803e12d55d4b16
Spink994 <ayodeji.bakare@sourcemygadgets.com>
2023-04-24 21:37:20 +0100

    Added Notfound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import routes from "./components/Sidebar/routes.const";
 import Dashboard from "./pages/Dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Notfound from "./pages/Notfound";
 
 // When pages begin to increase, it will become necessary to implement code splitting for better performance
 
@@ -10,6 +11,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path={routes.dashboard} element={<Dashboard />} />
+          <Route path="/*" element={<Notfound />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/Notfound/index.tsx
@@ -0,0 +1,7 @@
+export default function Notfound() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-zinc-900 text-white">
+      404  -  Page does not exist
+    </div>
+  );
+}
